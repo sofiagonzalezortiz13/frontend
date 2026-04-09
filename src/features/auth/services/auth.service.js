b8047d4f29984cadd4d344b151628aabedece976
sofiagonzalesortiz13 <sofiagonzalezort13@gmail.com>
2026-04-09 09:24:04 -0500

    edi

--- src/features/auth/services/auth.service.js
+++ src/features/auth/services/auth.service.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// Configura la URL base de tu backend (ajusta el puerto si es necesario)
+// Configura la URL base de tu backend
 const API_URL = "https://backend-e6v4.onrender.com/api";
 
-// ✅ EXPORTAR LOGIN
+// ✅ EXPORTAR LOGIN corregido con /auth
 export const loginRequest = async (user) => {
-  return await axios.post(`${API_URL}/login`, user);
+  return await axios.post(`${API_URL}/auth/login`, user);
 };
 
-// ✅ EXPORTAR REGISTER (Esta es la que te falta)
+// ✅ EXPORTAR REGISTER corregido con /auth
 export const registerRequest = async (user) => {
-  return await axios.post(`${API_URL}/register`, user);
+  return await axios.post(`${API_URL}/auth/register`, user);
 };
